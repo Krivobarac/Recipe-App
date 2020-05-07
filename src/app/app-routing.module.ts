5656de19914eea904915c53d5be3ad27cebf1d8c
Krivobarac <gasparovicsasa@yahoo.com>
2020-05-07 16:09:40 +0200

    data binding beetwen recipes component and its child components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RecipesComponent } from './components/recipes/recipes.component';
+import { ShopingListComponent } from './components/shoping-list/shoping-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/recipes', pathMatch: 'full'},
+  {path: 'recipes', component: RecipesComponent},
+  {path: 'shopingList', component: ShopingListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
